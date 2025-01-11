b1724c7cc6fd79e2ceb2a1a1164639f178e5ac1c
vinsentli <vinsentli@tencent.com>
2025-01-11 18:38:11 +0800

    Create soft link for NanovgSession.*

--- prepare.sh
+++ prepare.sh
@@ -1,2 +1,10 @@
 cp -af patch/ igl/
-cp example/NanovgSession.* igl/shell/renderSessions/
+
+file_list=(NanovgSession.h NanovgSession.cpp)
+
+#create soft link
+for file in "${file_list[@]}";do
+	dst_file=igl/shell/renderSessions/${file}
+	test -f ${dst_file} && rm ${dst_file}
+	ln -s $(pwd)/example/${file} ${dst_file}
+done
